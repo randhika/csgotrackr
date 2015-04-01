28d30624f9e4ae124cb2f8c6eb24b60608891fc7
Possimpible <Possimpible@users.noreply.github.com>
2015-04-01 18:02:30 +0200

    New Fragment

--- app/src/main/java/com/example/android/cstogo/MyPagerAdapter.java
+++ app/src/main/java/com/example/android/cstogo/MyPagerAdapter.java
@@ -14,14 +14,34 @@ public class MyPagerAdapter extends FragmentPagerAdapter {
     }
     @Override
     public CharSequence getPageTitle(int position) {
-        return (position == 0)? "My Matches" : "Stats" ;
+        switch (position) {
+            case 0:
+                return "My Matches";
+            case 1:
+                return "Stats";
+            case 2:
+                return "All Games";
+            default:
+                return null;
+        }
+        //return (position == 0)? "My Matches" : "Stats" ;
     }
     @Override
     public int getCount() {
-        return 2;
+        return 3;
     }
     @Override
     public Fragment getItem(int position) {
-        return (position == 0)? new FragmentA() : new FragmentB() ;
+        switch (position) {
+            case 0:
+                return new FragmentA();
+            case 1:
+                return new FragmentB();
+            case 2:
+                return new FragmentC();
+            default:
+                return null;
+        }
+        //return (position == 0)? new FragmentA() : new FragmentB() ;
     }
 }
